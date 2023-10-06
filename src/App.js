e410562a5d92a7fcc25f025fc3f8324729ab7732
anotherrandomonline <113223065+anotherrandomonline@users.noreply.github.com>
2023-10-06 10:59:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {evaluate} from 'mathjs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      num1: 0,
+      num2: 0,
+      type: '+',
+      result: ''
+    }
+    this.calculate = this.calculate.bind(this)
+  }
+  set(event) {
+    let name = event.target.name
+    let value = event.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+  calculate() {
+    let {num1, num2, type} = this.state
+    this.setState({
+      result: evaluate(`${num1}${type}${num2}`)
+    })
+  }
+  render() {
+    return (
+      <div className='mx-auto pt-3' style={{width: 400, textAlign:'center'}}>
+        <h1>
+          {this.state.num1} {this.state.type} {this.state.num2} = {this.state.result}
+        </h1>
+        <input name='num1' placeholder='First Number' type="number" onChange={this.set.bind(this)} />
+        <input name='num2' placeholder='Second Number' type="number" onChange={this.set.bind(this)} /><br /><br />
+        <button type="button" className="btn btn-primary" onClick={() => this.setState({type: "+"})}>Addition</button>
+        <button type="button" className="btn btn-primary" onClick={() => this.setState({type: "-"})}>Subtraction</button>
+        <button type="button" className="btn btn-primary" onClick={() => this.setState({type: "*"})}>Multiplication</button>
+        <button type="button" className="btn btn-primary" onClick={() => this.setState({type: "/"})}>Division</button><br /><br />
+        <button type="button" className="btn btn-primary" onClick={this.calculate}>Calculate</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
